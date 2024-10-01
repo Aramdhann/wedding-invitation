bef3b2cfed85fd76928fd6a30d58d577ad680f0b
Aramdhann <radityamochamad@gmail.com>
2024-10-01 08:26:53 +0700

    feat: add dynamic name-based greeting for wedding invitations using route params
    - Updated route with optional 'name' parameter for personalized invitations
    - Modified OpeningWord component to display custom message based on URL parameter
    - Display default message for uninvited users if no name is provided in the URL

--- src/components/OpeningWord.vue
+++ src/components/OpeningWord.vue
@@ -1,8 +1,35 @@
 <template>
-  <p class="absolute flex items-center justify-center z-10 text-white text-3xl custom-font-kyiv ">
-    Hai Semua
-  </p>
+  <div
+    id="OpeningWord"
+    class="absolute flex flex-col items-center justify-center z-10 opacity-0 transition-opacity duration-300"
+  >
+    <p class="text-white text-2xl custom-font-kyiv text-center w-3/4">
+      <span v-if="name">Halo, {{ name }} You are invited to our wedding!</span>
+      <span v-else>Sorry, you are not invited to this wedding</span>
+    </p>
+    <button v-if="name"
+      class="px-10 py-2 bg-[#B2CAB5] rounded-full mt-[130px] duration-500 hover:bg-[#A0B8A4] hover:scale-105 transform transition-all"
+    >
+      Buka Undangan
+    </button>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const name = route.params.name
+
+onMounted(() => {
+  setTimeout(() => {
+    const openingWord = document.getElementById('OpeningWord')
+    if (openingWord) {
+      openingWord.classList.add('opacity-100')
+    }
+  }, 2500)
+})
+</script>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 
 const routes = [
-  { path: '/', name:'home', component: HomeView },
+  { path: '/:name?', name:'home', component: HomeView },
   { path: '/about', name:'about', component: AboutView },
 ]
 
